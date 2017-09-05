519268d536d4fe81e7726e05f9386fed33ddbed1
ajeetsingh-github <admin@Ajeetsingh.AD.SAASFOCUS.COM>
2017-09-05 16:03:10 +0530

    changes done

---
+++ force-app/main/default/aura/quickContact/quickContactController.js
@@ -0,0 +1,14 @@
+({
+    myAction : function(component, event, helper) {
+
+    },
+
+
+    // function to handle the "Save" button click
+    handleSave : function(component, event, helper) {
+        //invoke Data Service's inbuilt saveRecord function
+        component.find("DataService").saveRecord(function(result){});
+    }
+
+
+})
